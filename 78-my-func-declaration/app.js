dad2a6945cf12ace46980cfe30c99d17d8b2b736
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-03-31 00:07:25 +0500

    first commit

--- 78-my-func-declaration/app.js
+++ 78-my-func-declaration/app.js
@@ -1,1 +1,11 @@
 "use strict";
+//function declaration means define the function with its name
+function squareNumber(a, b) {
+    return a * b;
+}
+;
+console.log(squareNumber(3, 5));
+//function expression means define function with its value
+let squarenumber = function (a, b) {
+    return a * b;
+};
--- 78-my-func-declaration/app.ts
+++ 78-my-func-declaration/app.ts
@@ -1,2 +1,12 @@
-//function declaration means define the function
-function squareDeclare(number:number)
+//function declaration means define the function with its name
+
+function squareNumber(a:number , b:number):number{
+    return a * b
+};
+console.log(squareNumber(3 , 5));
+//function expression means define function with its value
+let squarenumber = function(a : number , b : number){
+   return a * b
+};
+
+
